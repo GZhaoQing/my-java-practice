868d7c5aff47097f459244b9e258a07cbf4635f3
guozhaoqing <ufoguozhaoqing@qq.com>
2022-01-13 17:43:40 +0800

    增加2个脱敏正则

--- src/main/java/com/ufo/regex/UsefulPattens.java
+++ src/main/java/com/ufo/regex/UsefulPattens.java
@@ -16,8 +16,21 @@ public class UsefulPattens {
 
     public static Pattern FILE_NAME_IN_PATH_NO_TYPE = Pattern.compile("[^\\\\/]+(?=\\.\\w+$)");//捕捉文件路径结尾的文件名,没有扩展名
 
+    public static Pattern SAFE_MASK_IDCARD=Pattern.compile("(\\d{6})\\d*(\\w{4})");
+    public static Pattern SAFE_MASK_TELPHONE=Pattern.compile("(\\d{3})\\d*(\\d{4})");
 
     public static void main(String[] args){
+        Matcher m = SAFE_MASK_IDCARD.matcher("32010220010101123X");
+//        m = SAFE_MASK_IDCARD.matcher("320102700101123");
+        String s = m.replaceAll("$1********$2");
+        System.out.printf("SAFE_MASK_IDCARD:%s",s);
+        System.out.print("\n");
+        Matcher m2 = SAFE_MASK_TELPHONE.matcher("11045641345");
+        String s2 = m2.replaceAll("$1****$2");
+        System.out.printf("SAFE_MASK_TELPHONE:%s",s2);
+    }
+
+    public static void testFileMasks(){
         Matcher matcher = FILE_NAME_IN_PATH.matcher("/doc/profile.d/file.s.x");
         System.out.println(matcher.find()+":"+matcher.group());
         Matcher matcher2 = FILE_NAME_IN_PATH_NO_TYPE.matcher("/doc/profile.d/file.s.jpg");
@@ -33,5 +46,4 @@ public class UsefulPattens {
         //简写形式也支持分组
         System.out.println("mailhost@amail.com".replaceAll("\\b(\\w[-.\\w]*@[-\\w]+(\\.[\\w]+)*\\.(com|edu|info))\\b","<a href=\"mailto:$1\">$1</a>"));
     }
-
 }
